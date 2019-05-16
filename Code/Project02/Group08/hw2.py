8c4d0add0ab3f2de9ef51992faafd104bb0fc205
isu10603105a <isu10603105a@isu.edu.tw>
2019-05-16 18:32:18 +0800

    project2 modify

--- Code/Project02/Group08/hw2.py
+++ Code/Project02/Group08/hw2.py
@@ -2,14 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
+import hw1v2
 import time
 import re
+
 chrome_options = Options()
 chrome_options.add_argument('--headless')
 
-# 啟動webdrive 這裡使用chromedriver需要另外下載
+# 啟動webdrive
 driver = webdriver.Chrome(
-    'C:\\Users\\JK251\\Desktop\\NLP_homework\\HW2\\chromedriver.exe')
+    'chromedriver.exe')
 # get義大醫院網頁
 driver.get('https://www5.edah.org.tw/MainDept.aspx?Hospital=EDAH')
 # 各科的連結網址 這裡是皮膚科
@@ -21,21 +23,22 @@ soup = BeautifulSoup(driver.page_source, 'html5lib')
 # 搜尋table
 table = soup.find('table', id='AutoNumber1')
 # 關閉webdrive
-driver.close()
+driver.quit()
 # print(table.prettify())
 while True:
-    print("你想知道義大醫院 哪位皮膚科醫生的看診時間？ 如要結束請輸入 q")
-    search_name = input(" 請直接輸入醫生姓名 或是 輸入q來結束：  ")
+    # print("你想知道義大醫院 哪位皮膚科醫生的看診時間？ 如要結束請輸入 q")
+    search_name = input(" 請輸入 或是 輸入q來結束：")
     if search_name == "q":
         break
-
+    search_name = hw1v2.findtext(search_name)
     # 搜尋列表
     rows = [x for x in table.findAll('tr') if x.find("td") != None]
 
     data_list = []
-    for i in range(0, len(rows)-1):
+    for i in range(0, len(rows) - 1):
         for j in range(1, 4):
-            if re.search(search_name, rows[i].findAll('td')[j].text) != None and re.search("已額滿", rows[i].findAll('td')[j].text) == None:
+            if re.search(search_name, rows[i].findAll('td')[j].text) != None and re.search("已額滿", rows[i].findAll('td')[
+                j].text) == None:
                 data_list.append({
                     '醫師姓名': search_name,
                     '日期': rows[i].findAll('td')[0].text,
